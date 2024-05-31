9a342779b2ec5f81aa62b45a7586f4f2c2e0887d
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-05-31 16:14:15 +0530

    Correct email parsing to prevent malformed tracking links, issue#37

--- app/src/main/java/com/priyank/drdelivery/shipmentDetails/domain/ParseEmail.kt
+++ app/src/main/java/com/priyank/drdelivery/shipmentDetails/domain/ParseEmail.kt
@@ -8,69 +8,53 @@ import com.google.api.services.gmail.model.MessagePart
 import com.priyank.drdelivery.shipmentDetails.domain.model.InterestingEmail
 import java.util.Date
 
-class ParseEmail() {
+class ParseEmail {
 
     // #TODO
     // Add more pattern to find link from Amazon,Myntra,Ajio etc
     // Also refactor the code to make so that new pattern can be added easily in future...
 
-    private fun getTextFromBodyPart(
-        bodyPart: MessagePart
-    ): String {
-
-        var result: String = ""
-        if (bodyPart.mimeType == "text/plain") {
-
-            val r = bodyPart.body.data
-            result =
-                StringUtils
-                    .newStringUtf8(Base64.decodeBase64(r))
-        } else if (bodyPart.mimeType == "text/html") {
-            val html = bodyPart.body.data
-            val s = StringUtils
-                .newStringUtf8(Base64.decodeBase64(html))
-
-            result = s
+    private fun getTextFromBodyPart(bodyPart: MessagePart): String {
+        var result = ""
+        if (bodyPart.mimeType == "text/plain" || bodyPart.mimeType == "text/html") {
+            val data = bodyPart.body.data
+            result = StringUtils.newStringUtf8(Base64.decodeBase64(data))
         }
-
         return result
     }
 
     fun parseEmail(email: Message): InterestingEmail? {
+        var parsedEmail = ""
 
-        val emailSize = email.payload.parts?.size ?: 0
-        var parsedEmail = " "
-
-        for (k in 0 until emailSize) {
-            parsedEmail += getTextFromBodyPart(email.payload.parts[k])
+        if (email.payload.parts != null) {
+            for (part in email.payload.parts) {
+                parsedEmail += getTextFromBodyPart(part)
+            }
+        } else {
+            parsedEmail += getTextFromBodyPart(email.payload)
         }
 
-        val FlipkartPattern =
-            Regex("http:\\/\\/delivery\\..+?\\.flipkart\\.com\\/([A-Za-z0-9\\?\\=&\\/\\\\\\+]+)")
-        val isLinkpresent = FlipkartPattern.containsMatchIn(parsedEmail)
+        val flipkartPattern = Regex("http://delivery\\.ncp\\.flipkart\\.com/[A-Za-z0-9?=&/\\\\+]+")
+        val matchResult = flipkartPattern.find(parsedEmail)
 
-        if (isLinkpresent) {
-
-            Log.d("EMAIL ${timeEmailWasRecieved(email)}", parsedEmail)
-
-            return InterestingEmail(
+        return if (matchResult != null) {
+            val foundLink = matchResult.value
+            Log.d("EMAIL ${timeEmailWasReceived(email)}", parsedEmail)
+            InterestingEmail(
                 email.id,
-                FlipkartPattern.find(parsedEmail)!!.value,
+                foundLink,
                 "Flipkart",
-                timeEmailWasRecieved(email),
+                timeEmailWasReceived(email),
                 null
             )
         } else {
-
-            Log.d(" LINK NOT FOUND IN EMAIL ${timeEmailWasRecieved(email)}", parsedEmail)
-
-            return null
+            Log.d("LINK NOT FOUND IN EMAIL ${timeEmailWasReceived(email)}", parsedEmail)
+            null
         }
     }
 
-    fun timeEmailWasRecieved(email: Message): String {
-
-        val timeInMillis = email.get("internalDate") as String
+    private fun timeEmailWasReceived(email: Message): String {
+        val timeInMillis = email["internalDate"] as String
         val netDate = Date(timeInMillis.toLong()).toLocaleString()
         return netDate
     }
